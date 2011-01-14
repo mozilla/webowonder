853326de769e79637cf01d26f908e7bdb6e620f1
Austin King <shout@ozten.com>
2011-01-14 11:05:51 -0800

    Switching to {0} and format. Adding L10n comments

--- apps/wow/views.py
+++ apps/wow/views.py
@@ -5,11 +5,12 @@ from django import http
 
 import jingo
 
-from tower import ugettext as _, ungettext as _n
+from tower import ugettext as _, ungettext as ngettext
 
 from demos.models import Submission
 
 tags = {
+    # L10n: Technology Tags. A demo can have up to 5 of these. Keep them short.
     'Audio': _('Audio'),
     'Canvas': _('Canvas'),
     'CSS3': _('CSS3'),
@@ -25,6 +26,7 @@ tags = {
     'SVG': _('SVG'),
     'Video': _('Video'),
     'WebGL': _('WebGL'),
+     # L10n: Last of the Technology Tags.
     'XMLHttpRequest': _('XMLHttpRequest'),
 }
 
@@ -34,17 +36,27 @@ def home(request):
     for demo in data['demos']:
 
         ltags = [tags[x.strip()] for x in demo.tags.split(',')]
-
-        t = _n('Built with <a href="#">%s</a>.',
-               'Built with <a href="#">%s</a>, and <a href="#">%s</a>',
+        # L10n: TagList: (see also Tag Glue)
+        # There will never be a case with 0 Tags
+        # One Tag - {0} is replaced with a Tag.
+        # Two Tags- {0} is replaced with the first tag. {1} is replaced with the 
+        #           second tag.
+        # Three or more - {0} is replaced with a list of Tags glued together with 
+        #           the Tag Glue. {1} is replaced with the last tag.
+        #           Be careful not to remove the <a href='#'> and </a> HTML.
+        t = ngettext("Built with <a href='#'>{0}</a>.",
+               "Built with <a href='#'>{0}</a>, and <a href='#'>{1}</a>.",
                len(ltags))
-        glue = '</a>, <a href="#">'
+        # L10n: Tag Glue
+        #       HTML and punctuation used to glue the Tag List together.
+        glue = _("</a>, <a href='#'>")
 
         if len(ltags) < 2:
-            demo.tag_copy = t % ltags[-1]
+            demo.tag_copy = t.format(ltags[0])
         else:
-            demo.tag_copy = t % (glue.join(ltags[0:-1]), ltags[-1]) 
-        demo.video_title = _('The Making of %s') % demo.title
+            demo.tag_copy = t.format(glue.join(ltags[0:-1]), ltags[-1])
+        # L10n {0} is the title of a demo
+        demo.video_title = _('The Making of {0}').format(demo.title)
         # TODO new db field
         demo.video_description = 'See how John Smith brothers brought space down to earth.'
 
