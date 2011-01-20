83f752b780bdb1562e9b40c5ee56038f3f522591
Austin King <shout@ozten.com>
2011-01-20 09:26:55 -0800

    Followed Stas L10n advice. Synced webowonder-demos with migration and tags.

--- apps/wow/views.py
+++ apps/wow/views.py
@@ -1,33 +1,51 @@
 # -*- coding: utf-8 -*-
 """Example views. Feel free to delete this app."""
 
-from django import http
+import itertools
 
 import jingo
 
-from tower import ugettext as _, ungettext as ngettext
+from tower import ugettext as _
 
 from demos.models import Submission
 
+# TODO, need actual urls and such
 tags = {
+    # L10n: Tag MDN urls. You can change these if your locale has a better url
+    'Audio':       (_('http://developer.mozilla.org/En/Audio'), 
     # L10n: Technology Tags. A demo can have up to 5 of these. Keep them short.
-    'Audio': _('Audio'),
-    'Canvas': _('Canvas'),
-    'CSS3': _('CSS3'),
-    'DnD': _('DnD'),
-    'Files': _('Files'),
-    'Fonts': _('Fonts'),
-    'Forms': _('Forms'),
-    'GeoLocation': _('GeoLocation'),
-    'JavaScript': _('JavaScript'),
-    'HTML5': _('HTML5'),
-    'Mobile': _('Mobile'),
-    'MultiTouch': _('MultiTouch'),
-    'SVG': _('SVG'),
-    'Video': _('Video'),
-    'WebGL': _('WebGL'),
+                    _('Audio')),
+    'Canvas':      (_('http://developer.mozilla.org/En/Canvas'), 
+                    _('Canvas')),
+    'CSS3':        (_('http://developer.mozilla.org/En/CSS'),
+                    _('CSS3')),
+    'DnD':         (_('http://developer.mozilla.org/En/DnD'),
+                    _('DnD')),
+    'Files':       (_('http://developer.mozilla.org/En/Files'),
+                    _('Files')), 
+    'Fonts':       (_('http://developer.mozilla.org/En/Fonts'),
+                    _('Fonts')),
+    'Forms':       (_('http://developer.mozilla.org/En/Forms'),
+                    _('Forms')),
+    'GeoLocation': (_('http://developer.mozilla.org/En/GeLocation'), 
+                    _('GeoLocation')),
+    'JavaScript':  (_('http://developer.mozilla.org/En/JavaScript'), 
+                    _('JavaScript')), 
+    'HTML5':       (_('http://developer.mozilla.org/En/HTML5'),
+                    _('HTML5')), 
+    'Mobile':      (_('http://developer.mozilla.org/En/Mobile'),
+                    _('Mobile')), 
+    'MultiTouch':  (_('http://developer.mozilla.org/En/MultiTouch'),
+                    _('MultiTouch')),
+    'SVG':         (_('http://developer.mozilla.org/En/SVG'), 
+                    _('SVG')), 
+    'Video':       (_('http://developer.mozilla.org/En/Video'),
+                    _('Video')), 
+    'WebGL':       (_('http://developer.mozilla.org/En/WebGL'),
+                    _('WebGL')), 
      # L10n: Last of the Technology Tags.
-    'XMLHttpRequest': _('XMLHttpRequest'),
+    'XMLHttpRequest': ('http://developer.mozilla.org/En/',
+                       _('XMLHttpRequest'))
 }
 
 def home(request):
@@ -35,26 +53,28 @@ def home(request):
     data = {'demos': Submission.objects.filter(hidden=False)}
     for demo in data['demos']:
 
-        ltags = [tags[x.strip()] for x in demo.tags.split(',')]
-        # L10n: TagList: (see also Tag Glue)
-        # There will never be a case with 0 Tags
-        # One Tag - {0} is replaced with a Tag.
-        # Two Tags- {0} is replaced with the first tag. {1} is replaced with the 
-        #           second tag.
-        # Three or more - {0} is replaced with a list of Tags glued together with 
-        #           the Tag Glue. {1} is replaced with the last tag.
-        #           Be careful not to remove the <a href='#'> and </a> HTML.
-        t = ngettext("Built with <a href='#'>{0}</a>.",
-               "Built with <a href='#'>{0}</a>, and <a href='#'>{1}</a>.",
-               len(ltags))
-        # L10n: Tag Glue
-        #       HTML and punctuation used to glue the Tag List together.
-        glue = _("</a>, <a href='#'>")
+        ltags = [(tags[x.strip()][0], tags[x.strip()][1],) for x in demo.tags.split(',')]
+        tag_pairs = list(itertools.chain(*ltags))
 
-        if len(ltags) < 2:
-            demo.tag_copy = t.format(ltags[0])
-        else:
-            demo.tag_copy = t.format(glue.join(ltags[0:-1]), ltags[-1])
+        # This may be ugly to a programmer, but the simplicity
+        # is for ease of L10n. Please don't make this more terse
+        copy = None
+        if len(ltags) == 1:
+            # L10n {1} is a tag like HTML5 or GeoLocation {0} is a url
+            copy = _("Built with <a href='{0}'>{1}</a>.")
+        elif len(ltags) == 2:
+            # L10n {1} and {3} are tags like HTML5 and GeoLocation, {0} and {2} are urls
+            copy = _("Built with <a href='{0}'>{1}</a> and <a href='{2}'>{3}</a>.")
+        elif len(ltags) == 3:
+            copy = _("Built with <a href='{0}'>{1}</a>, <a href='{2}'>{3}</a> and <a href='{4}'>{5}</a>.")
+        elif len(ltags) == 4:
+            copy = _("Built with <a href='{0}'>{1}</a>, <a href='{2}'>{3}</a>, <a href='{4}'>{5}</a> and <a href='{6}'>{7}</a>.")
+        elif len(ltags) == 5:
+            copy = _("Built with <a href='{0}'>{1}</a>, <a href='{2}'>{3}</a>, <a href='{4}'>{5}</a>, <a href='{6}'>{7}</a> and <a href='{8}'>{9}</a>.")
+
+        if copy:
+            demo.tag_copy = copy.format(*tag_pairs)
+
         # L10n {0} is the title of a demo
         demo.video_title = _('The Making of {0}').format(demo.title)
         # TODO new db field
