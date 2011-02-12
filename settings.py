060df6c42a4cc948eea9dc893030eec69456a162
Austin King <shout@ozten.com>
2011-02-12 13:11:22 -0800

    Enabling lots of locales

--- settings.py
+++ settings.py
@@ -47,7 +47,7 @@ USE_L10N = True
 LANGUAGE_CODE = 'en-US'
 
 # Accepted locales
-KNOWN_LANGUAGES = ('en-US','de','pl')
+KNOWN_LANGUAGES = ('en-US','cs', 'de','el', 'fy', 'ga', 'he', 'id', 'pl', 'rm', 'ru', 'sl')
 
 # List of RTL locales known to this project. Subset of LANGUAGES.
 RTL_LANGUAGES = ()  # ('ar', 'fa', 'fa-IR', 'he')
