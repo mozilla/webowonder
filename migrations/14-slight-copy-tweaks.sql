50f5f7423029cfcd3104f0cd2a9f6ff074e4a0d0
Austin King <shout@ozten.com>
2011-02-16 08:55:50 -0800

    Bug#632559 Suprise, copy changes for the final copy. Shocking.

---
+++ migrations/14-slight-copy-tweaks.sql
@@ -0,0 +1,9 @@
+UPDATE demos_submission SET description = "Add real-time, immersive effects to your videos with WebGL." WHERE slug = "remixingreality";
+
+UPDATE demos_submission SET description = "CSS3 sheds light on familiar 'characters'." WHERE slug = "shadows";
+
+UPDATE demos_submission SET description = "Build awesome games without plugins! How fast can your fox run?" WHERE slug = "runfield";
+
+UPDATE demos_submission SET description = "An in-depth tutorial on Web technologies featured in Firefox 4." WHERE slug = "dashboard";
+
+UPDATE demos_submission SET description = "Explore a beautiful CSS3 version of our universe." WHERE slug = "planetarium";
