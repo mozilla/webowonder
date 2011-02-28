3cbcf2f990418fc790baeb1e5c3efc6d14d0bc27
Austin King <shout@ozten.com>
2011-02-28 10:52:19 -0800

    Hidding demos for Phase I launch.

---
+++ migrations/17-phase-I-launch.sql
@@ -0,0 +1,8 @@
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'shadows';
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'immersivevideo';
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'globetweeter';
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'runfield';
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'flight-of-the-navigator';
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'dashboard';
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'planetarium';
+UPDATE demos_submission SET hidden = b'1' WHERE slug = 'nocomply';
