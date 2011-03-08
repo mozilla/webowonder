2939433d6d5fa4ed5a50a1a4339ae5c4ba6284bc
Austin King <shout@ozten.com>
2011-03-08 13:29:14 -0800

    folding in master's migration #23, bumping 24, 25

---
+++ migrations/23-source-urls.sql
@@ -0,0 +1,8 @@
+
+UPDATE demos_submission SET source_code_url = 'https://github.com/simurai/shadows/' WHERE slug = 'shadows';
+
+UPDATE demos_submission SET source_code_url = 'https://github.com/paulrouget/motivational/' WHERE slug = 'motivational';
+
+UPDATE demos_submission SET source_code_url = 'https://github.com/cedricpinson/globetweeter/' WHERE slug = 'globetweeter';
+
+UPDATE demos_submission SET source_code_url = 'https://github.com/paulrouget/immersivevideo/' WHERE slug = 'immersivevideo';
