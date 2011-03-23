6019676bddc0d1271c57f482051423e4be536f0b
Austin King <shout@ozten.com>
2011-03-23 09:16:21 -0700

    Switching to production l10n languages

--- settings.py
+++ settings.py
@@ -48,12 +48,10 @@ LANGUAGE_CODE = 'en-US'
 
 # Accepted locales
 # Staging
-KNOWN_LANGUAGES = ('en-US', 'ar', 'ca', 'cs', 'de','el', 'es', 'eu', 'fr', 'fy', 'ga', 'he', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'pl', 'pt', 'rm', 'ro', 'ru', 'si', 'sl', 'sr', 'sq', 'th', 'tr', 'zh-CN', 'zh-TW')
+#KNOWN_LANGUAGES = ('en-US', 'ar', 'ca', 'cs', 'de','el', 'es', 'eu', 'fr', 'fy', 'ga', 'he', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'pl', 'pt', 'rm', 'ro', 'ru', 'si', 'sl', 'sr', 'sq', 'th', 'tr', 'zh-CN', 'zh-TW')
 
 # Production
-#KNOWN_LANGUAGES = ('en-US', 'ar', 'ca', 'cs', 'de', 'el', 'es', 'fr', 'fy',
-#'ga', 'he', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'pl', 'pt', 'rm', 'ru', 'si',
-#'sl', 'sq', 'th', 'tr', 'zh-CN', 'zh-TW')
+KNOWN_LANGUAGES = ('en-US', 'ar', 'ca', 'cs', 'de', 'el', 'es', 'fr', 'fy', 'ga', 'he', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'pl', 'pt', 'rm', 'ru', 'si', 'sl', 'sq', 'th', 'tr', 'zh-CN', 'zh-TW')
 
 # List of RTL locales known to this project. Subset of LANGUAGES.
 RTL_LANGUAGES = ('ar',)  # ('ar', 'fa', 'fa-IR', 'he')
