9bc360bb6c14ec75c4dc54c560fbd7e11b216621
Austin King <shout@ozten.com>
2011-03-23 11:41:24 -0700

    Bug#644267 Updating github urls for nocomply and fotn

---
+++ migrations/33-github-links.sql
@@ -0,0 +1,3 @@
+UPDATE demos_submission SET source_code_url = 'https://github.com/humphd/nocomply' WHERE slug = 'nocomply';
+
+UPDATE demos_submission SET source_code_url = 'https://github.com/humphd/fotn' WHERE slug = 'flight-of-the-navigator';
