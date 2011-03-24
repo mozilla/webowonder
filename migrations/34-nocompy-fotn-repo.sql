5611f53400495db7496c0ef057f91f353d5de8bb
Austin King <shout@ozten.com>
2011-03-24 10:53:19 -0700

    Bug#644651 updating source urls nocomply fotn

---
+++ migrations/34-nocompy-fotn-repo.sql
@@ -0,0 +1,3 @@
+UPDATE demos_submission SET source_code_url = 'https://github.com/cjcliffe/NoComply' WHERE slug = 'nocomply';
+
+UPDATE demos_submission SET source_code_url = 'https://github.com/cjcliffe/FOTN' WHERE slug = 'flight-of-the-navigator';
